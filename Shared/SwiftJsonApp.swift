9698e83d817ab68cd7ccf4de0a6eb7347f6a9b46
Christopher Chee <ketyung@techchee.com>
2021-01-01 09:12:09 +0800

    Initial Commit

---
+++ Shared/SwiftJsonApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftJsonApp.swift
+//  Shared
+//
+//  Created by Chee Ket Yung on 01/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftJsonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
